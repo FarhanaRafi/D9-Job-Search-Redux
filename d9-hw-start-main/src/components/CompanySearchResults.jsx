c49b643f7378de513bc7dc961007039b2cdf3245
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-09 15:33:54 +0100

    features done

--- d9-hw-start-main/src/components/CompanySearchResults.jsx
+++ d9-hw-start-main/src/components/CompanySearchResults.jsx
@@ -7,7 +7,9 @@ const CompanySearchResults = () => {
   const [jobs, setJobs] = useState([]);
   const params = useParams();
 
-  const baseEndpoint = "https://strive-jobs-api.herokuapp.com/jobs?company=";
+  const baseEndpoint =
+    "https://strive-benchmark.herokuapp.com/api/jobs?company=";
+  // https://strive-jobs-api.herokuapp.com/jobs?company=
 
   useEffect(() => {
     getJobs();
--- d9-hw-start-main/src/components/Favorites.jsx
+++ d9-hw-start-main/src/components/Favorites.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FaTrash } from "react-icons/fa";
-import { Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Col, Row, Button, Container } from "react-bootstrap";
 
 const Favorites = () => {
   let favorite = useSelector((state) => state.favorites.content);
   const dispatch = useDispatch();
   return (
     <>
-      <h1>Favorites</h1>
-      <Row>
-        <Col sm={12}>
-          <ul style={{ listStyle: "none" }}>
-            {favorite.map((job, i) => (
-              <li key={i} className="my-4">
-                <Button
-                  variant="danger"
-                  onClick={() => {
-                    dispatch({
-                      type: "REMOVE_FROM_FAVORITE",
-                      payload: i,
-                    });
-                  }}
-                >
-                  <FaTrash />
-                </Button>
-
-                {job.title}
-              </li>
-            ))}
-          </ul>
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <h1>Favorites</h1>
+          <Col sm={12}>
+            <ul style={{ listStyle: "none" }}>
+              {favorite.map((job, i) => (
+                <li key={i} className="my-4">
+                  <Link to={`/${job.company_name}`}>{job.company_name} </Link>
+                  <Button
+                    className="ml-5"
+                    variant="danger"
+                    onClick={() => {
+                      dispatch({
+                        type: "REMOVE_FROM_FAVORITE",
+                        payload: i,
+                      });
+                    }}
+                  >
+                    <FaTrash />
+                  </Button>
+                  <Button className="ml-5">Details</Button>
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
