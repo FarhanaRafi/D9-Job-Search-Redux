f1487c9fbf7cf9fb5c5eda9c3fce752284740040
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-09 16:42:47 +0100

    done

--- d9-hw-start-main/src/components/Favorites.jsx
+++ d9-hw-start-main/src/components/Favorites.jsx
@@ -4,8 +4,12 @@ import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { Button, Container, Table } from "react-bootstrap";
 import { format, parseISO } from "date-fns";
+import { useState } from "react";
+import { MDBCollapse, MDBBtn } from "mdb-react-ui-kit";
 
 const Favorites = () => {
+  const [showShow, setShowShow] = useState(false);
+  const toggleShow = () => setShowShow(!showShow);
   let favorite = useSelector((state) => state.favorites.content);
   const dispatch = useDispatch();
   return (
@@ -15,28 +19,40 @@ const Favorites = () => {
         <Table hover>
           <tbody>
             {favorite.map((job, i) => (
-              <tr key={i}>
-                <td>
-                  <Link to={`/${job.company_name}`}>{job.company_name} </Link>
-                </td>
-                <td>{job.title}</td>
-                <td>{job.category}</td>
-                <td>{format(parseISO(job.publication_date), `dd-LL-yyyy`)}</td>
-                <td>
-                  <Button
-                    className="ml-5"
-                    variant="danger"
-                    onClick={() => {
-                      dispatch({
-                        type: "REMOVE_FROM_FAVORITE",
-                        payload: i,
-                      });
-                    }}
-                  >
-                    <FaTrash />
-                  </Button>
-                </td>
-              </tr>
+              <>
+                <tr key={i}>
+                  <td class="expand-button">
+                    <MDBBtn onClick={toggleShow}></MDBBtn>
+                  </td>
+                  <td>
+                    <Link to={`/${job.company_name}`}>{job.company_name} </Link>
+                  </td>
+                  <td>{job.title}</td>
+                  <td>{job.category}</td>
+                  <td>
+                    {format(parseISO(job.publication_date), `dd-LL-yyyy`)}
+                  </td>
+                  <td>
+                    <Button
+                      className="ml-5"
+                      variant="danger"
+                      onClick={() => {
+                        dispatch({
+                          type: "REMOVE_FROM_FAVORITE",
+                          payload: i,
+                        });
+                      }}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <MDBCollapse show={showShow}>
+                    {job.description.slice(80, 500)}
+                  </MDBCollapse>
+                </tr>
+              </>
             ))}
           </tbody>
         </Table>
